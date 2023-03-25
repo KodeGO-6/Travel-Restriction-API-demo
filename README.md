641bef9b4232ba2359a29a77ebcd59811e1d15bb
Kevin Balmores <balmoresk3v1n@gmail.com>
2023-03-25 20:20:26 +0800

    created README.md

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # Travel-Restriction-API-demo
+
+# [Travel Restrictions API documentation and examples](https://developers.amadeus.com/self-service/category/covid-19-and-travel-safety/api-doc/travel-restrictions/api-reference)
+
+## Register to Amadeus for Developers to get your API Key
+
+---
+### Read their documentations
+
+[Amadeus for Developers Github Page](https://github.com/amadeus4dev)
+
+---
+
+
+## How to run this project locally
+---
+Clone the repository.
+```git 
+git clone https://github.com/KodeGO-6/Travel-Restriction-API-demo.git
+cd Travel-Restriction-API-demo
+```
+
+Install the dependencies.
+```nodejs
+npm install
+```
+
+Create your own .env file inside the root directory.
+```
+AMADEUS_CLIENT_ID = YOUR_AMADEUS_API_KEY
+AMADEUS_CLIENT_SECRET = YOUR_AMADEUS_API_SECRET
+PORT = 5000
+```
+
+Run the Node server.
+```nodejs
+node app.js
+```
+
+Finally, open a browser and go to `http://localhost:5000`
+
+
+---
+
+NOTE: I'll try to create demo for the the other APIs in their catalog
+
+
+
--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const https = require('https');
 const Amadeus = require('amadeus');
 
 const app = express();
 
-require('dotenv').config();
+require('dotenv').config();     // this is used to load Environment Variables from a .env file
 
-
+// Initialize the AMADEUS APP API KEY
 const amadeus = new Amadeus({
     clientId: process.env.AMADEUS_CLIENT_ID,
     clientSecret: process.env.AMADEUS_CLIENT_SECRET
@@ -14,40 +13,44 @@ const amadeus = new Amadeus({
 
 const countries = require('./countries.json');
 
-let countryCode;
+var countryCode;    // Stores the country code of the searched country from the countries.json file
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs')       // this is used to generate dynamic HTML with javaScript
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html')
+    res.sendFile(__dirname + '/views/index.html')       // view the homepage
 })
 
 app.get('/search', (req, res) => {
-    const query = req.query.query.toLowerCase();
-    for(const [code, name] of Object.entries(countries)){
-        if (name.toLowerCase() === query){
-            countryCode = code;
-            return res.redirect('/covid');
+    // Search for the name of the country 
+    const query = req.query.query.toLowerCase();    // convert the search query to lower case
+    for(const [code, name] of Object.entries(countries)){   
+        if (name.toLowerCase() === query){          // checks if the country name inside 'countries.json' file is strictly equal to the search query
+            countryCode = code;                     // store the country code to a variable that will be used later
+            return res.redirect('/covid');          // redirect the page to the '/covid' page
         }
     }
 
-    return res.send('Country not found.');
+    return res.send('Country not found.');      // return if no match is found
 });
 
 app.get('/covid', (req, res) => {
+    // Making API CALL that returns a Promise that either resolves or rejects
     amadeus.dutyOfCare.diseases.covid19Report.get({
-        countryCode: countryCode
+        countryCode: countryCode    // pass the countryCode variable here
     }).then(response => {
-        const { name } = response.data.area
-        const { lastUpdate, text } = response.data.summary
-        const riskLevelText = response.data.diseaseRiskLevel.text
-        const hotspots = response.data.hotspots.text
-        const { covidDashboardLink, healthDepartmentSiteLink } = response.data.dataSources
-        const entryLastUpdate = response.data.areaAccessRestriction.entry.lastUpdate
-        const entryText = response.data.areaAccessRestriction.entry.text
-        const { ban, throughDate, referenceLink, exemptions } = response.data.areaAccessRestriction.entry
+        // Store the respone in their own variables
+        const { name } = response.data.area;
+        const { lastUpdate, text } = response.data.summary;
+        const riskLevelText = response.data.diseaseRiskLevel.text;
+        const hotspots = response.data.hotspots.text;
+        const { covidDashboardLink, healthDepartmentSiteLink } = response.data.dataSources;
+        const entryLastUpdate = response.data.areaAccessRestriction.entry.lastUpdate;
+        const entryText = response.data.areaAccessRestriction.entry.text;
+        const { ban, throughDate, referenceLink, exemptions } = response.data.areaAccessRestriction.entry;
 
+        // this will render the response into HTML elements
         res.render('index', {
             name: name,
             lastUpdate: lastUpdate,
@@ -73,6 +76,7 @@ app.listen(process.env.PORT || 5000, () => {
     console.log('Server is running on port 5000')
 })
 
+// this will just remove any unwanted <p> tags in the response
 function removeTags(data){
     return data.replace('<p>', '').replace('</p>', '')
 }
